b05fb0e948e12fd6cf3beaa4924e3ebe6b9ba777
Heni <ouni.hani.fcb@gmail.com>
2020-05-17 21:20:00 +0100

    adding the tasks retrieving api with documentation

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -51,37 +51,36 @@ const Task = require('../models/Task');
  *       
  */
 
-router.post('/task', (req,res)=>{
+router.post('/task', (req, res) => {
     console.log("Adding New Post...");
     console.log(req.body.candidate_email);
-    if(!req.body.candidate_email){
+    if (!req.body.candidate_email) {
         return res.status(400).json({
-            sucess : false,
+            sucess: false,
             message: "No email or email was not sent appropriately"
         });
-    }else if(!req.body
-.label){
+    } else if (!req.body.label) {
         return res.status(401).json({
-            sucess : false,
+            sucess: false,
             message: "Task label is required"
         });
-    }else{
+    } else {
         let task = new Task({
-            candidate_email : req.body.candidate_email,
-            label : req.body.label
+            candidate_email: req.body.candidate_email,
+            label: req.body.label
         });
 
-        task.save((err)=>{
+        task.save((err) => {
             if (err) return res.status(500).json({
-                sucess : false,
+                sucess: false,
                 message: "Task is not saved to the database"
             });
-            
+
             return res.status(200).json({
-                sucess : true,
+                sucess: true,
                 message: "Task is created"
             });
-            
+
         });
     }
 
@@ -123,60 +122,133 @@ router.post('/task', (req,res)=>{
  *         description: the id of the task needs to be specified
  *       404:
  *         description: Task Not Found!
+ *       405:
+ *         description: Error while getting task!
  *       500:
  *         description: Task is not saved to the database
  *       
  */
 
-router.patch('/check_task', (req,res)=>{
+router.patch('/check_task', (req, res) => {
     console.log("checking/unchecking a task...");
-    if(!req.body.task_id){
+    if (!req.body.task_id) {
         return res.status(400).json({
-            sucess : false,
+            sucess: false,
             message: "You have to specify the id of the task to check"
         });
-    }else{
-       
+    } else {
+
         let task = Task.findOne({
-            _id : req.body.task_id
-        }, (err,task)=>{
-            if(!task){
+            _id: req.body.task_id
+        }, (err, task) => {
+            if (err) {
+                return res.status(405).json({
+                    sucess: false,
+                    msg: "Error while getting task!"
+                });
+            } else if (!task) {
                 return res.status(404).json({
                     sucess: false,
                     msg: "Task Not Found!"
                 });
-            }else {
+            } else {
                 console.log(task.label);
-                if(task.isDone == true){
-                    task.isDone=false;
-                }else{
-                    task.isDone=true;
+                if (task.isDone == true) {
+                    task.isDone = false;
+                } else {
+                    task.isDone = true;
                 }
 
-                task.save((err)=>{
+                task.save((err) => {
                     if (err) return res.status(500).json({
-                        sucess : false,
+                        sucess: false,
                         message: "Task is not saved to the database"
                     });
-                    
+
                     return res.status(200).json({
-                        sucess : true,
-                        message: "Task is cheked"
+                        sucess: true,
+                        message: "Task status is changed"
                     });
-                    
+
                 });
             }
         });
-        
+
     }
 });
 
 
-router.get('/tasks', (req,res)=>{
+/**
+ * @swagger
+ * /tasks:
+ *   get:
+ *     description: This retrieves the list of check or unchecked tasks ( depends on which type the user of this api specifies) 
+ *     tags: [Tasks]
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         description: Body of the request
+ *         required: true
+ *         type: json
+ *         schema:
+ *          required:
+ *              - checked
+ *              - candidate_email
+ *          properties: 
+ *           checked:
+ *              type: boolean
+ *              required: true
+ *              description: this is made to specify which kind of posts to retrieve
+ *           candidate_email:
+ *              type: string
+ *              required: true
+ *              description: This is required so candidates won't mess up each other's tasks
+ *     responses:
+ *       200:
+ *         description: Task status is changed
+ *       400:
+ *         description: No email or email was not sent appropriately
+ *       401:
+ *         description: You have to specify which kind of tasks you want to get
+ *       500:
+ *         description: Error when retrieving tasks from the database
+ *       
+ */
 
+router.get('/tasks', (req, res) => {
+    console.log("getting tasks...");
+    if (!req.body.candidate_email) {
+        return res.status(400).json({
+            sucess: true,
+            message: "No email or email was not sent appropriately"
+        });
+    } else if (req.body.checked == undefined || req.body.checked == null){
+        return res.status(401).json({
+            sucess: false,
+            message: "You have to specify which kind of tasks you want to get"
+        });
+    } else {
+        let task = Task.find({
+            candidate_email: req.body.candidate_email,
+            isDone: req.body.checked
+        }, (err, tasks) => {
+            if (err) return res.status(500).json({
+                sucess: false,
+                message: "Error when retrieving tasks from the database"
+            });
+            return res.status(200).json({
+                sucess: true,
+                message: "retrieved successfully",
+                tasks : tasks
+            });
+        });
+    }
 
 });
 
 
- 
+
 module.exports = router;
